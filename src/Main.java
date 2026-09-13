1b5450a0f36c988ab7eb24ef25dcbdfea6d33830
aikyn <domik.wi.fi228@gmail.com>
2026-09-14 00:33:01 +0500

    Add MechanicalBuilder and OfficeBuilder

--- src/Main.java
+++ src/Main.java
@@ -30,3 +30,105 @@ class Keyboard {
     }
 }
 
+interface KeyboardBuilder {
+    KeyboardBuilder setSwitchType(SwitchType type);
+    KeyboardBuilder setKeycaps(KeycapMaterial material);
+    KeyboardBuilder setLayout(SizeLayout layout);
+    KeyboardBuilder setRGB(boolean hasRGB);
+    KeyboardBuilder setWireless(boolean isWireless);
+    Keyboard build();
+}
+
+class MechanicalBuilder implements KeyboardBuilder {
+    private SwitchType switchType;
+    private KeycapMaterial keycaps;
+    private SizeLayout layout;
+    private boolean hasRGB;
+    private boolean isWireless;
+
+    @Override
+    public KeyboardBuilder setSwitchType(SwitchType type) {
+        this.switchType = type;
+        return this; // Returning 'this' is required for the fluent API
+    }
+
+    @Override
+    public KeyboardBuilder setKeycaps(KeycapMaterial material) {
+        this.keycaps = material;
+        return this;
+    }
+
+    @Override
+    public KeyboardBuilder setLayout(SizeLayout layout) {
+        this.layout = layout;
+        return this;
+    }
+
+    @Override
+    public KeyboardBuilder setRGB(boolean hasRGB) {
+        this.hasRGB = hasRGB;
+        return this;
+    }
+
+    @Override
+    public KeyboardBuilder setWireless(boolean isWireless) {
+        this.isWireless = isWireless;
+        return this;
+    }
+
+    @Override
+    public Keyboard build() {
+        // Validation check
+        if (switchType == SwitchType.MEMBRANE) {
+            throw new IllegalArgumentException("Mechanical keyboards cannot use membrane switches!");
+        }
+        return new Keyboard(switchType, keycaps, layout, hasRGB, isWireless);
+    }
+}
+
+class OfficeBuilder implements KeyboardBuilder {
+    private SwitchType switchType = SwitchType.MEMBRANE; // Default value
+    private KeycapMaterial keycaps = KeycapMaterial.ABS;
+    private SizeLayout layout = SizeLayout.FULL_SIZE;
+    private boolean hasRGB = false;
+    private boolean isWireless = true;
+
+    @Override
+    public KeyboardBuilder setSwitchType(SwitchType type) {
+        this.switchType = type;
+        return this;
+    }
+
+    @Override
+    public KeyboardBuilder setKeycaps(KeycapMaterial material) {
+        this.keycaps = material;
+        return this;
+    }
+
+    @Override
+    public KeyboardBuilder setLayout(SizeLayout layout) {
+        this.layout = layout;
+        return this;
+    }
+
+    @Override
+    public KeyboardBuilder setRGB(boolean hasRGB) {
+        this.hasRGB = hasRGB;
+        return this;
+    }
+
+    @Override
+    public KeyboardBuilder setWireless(boolean isWireless) {
+        this.isWireless = isWireless;
+        return this;
+    }
+
+    @Override
+    public Keyboard build() {
+        if (hasRGB) {
+            System.out.println("Warning: Office keyboards usually don't have RGB.");
+        }
+        return new Keyboard(switchType, keycaps, layout, hasRGB, isWireless);
+    }
+}
+
